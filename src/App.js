779c14312ab219675a7123fb8458b2670e9b312f
sudarshanuprety42 <suprety4444@gmail.com>
2019-08-24 14:51:32 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+// import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+//
+// function App(){
+//   return(
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" />
+//         <p>Learn react with Sudarshan Uprety</p>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
+
+import React from "react";
+import logo from "./todo-logo.png";
+import "./App.css";
+
+class App extends React.Component{
+
+  constructor(props){
+      super(props);
+      this.state={
+        newItem:"",
+        list:[]
+      };
+  }
+
+  addItem(todoValue){
+    if(todoValue !== ""){
+      const newItem = {
+        id: Date.now(),
+        value: todoValue,
+        isDone: false
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: ""
+      });
+    }
+  }
+
+  deleteItem(id){
+    const list = [...this.state.list];
+    const updatedlist = list.filter(item => item.id !== id);
+    this.setState({
+      list:updatedlist
+    });
+  }
+
+  updateInput(input){
+    this.setState({newItem:input});
+  }
+  checkItem(item){
+    const list = [...this.state.list];
+
+    for (var i = 0; i < list.length; i++) {
+      if(list[i].id===item.id){
+        list[i].isDone = !list[i].isDone;
+        break;
+      }
+    }
+    this.setState({
+      list
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <img  src={logo} width="100" height="100" className="logo" />
+        <h1 class="app-title">Sudarshan TODO App</h1>
+        <div className="container">
+          Add an Item....
+          <br />
+          <input type="text" className="input-text" placeholder="Write TODO" required
+          value={this.state.newItem}
+          onChange={e => this.updateInput(e.target.value)}
+          />
+          <button className="add-btn"
+          onClick={()=>this.addItem(this.state.newItem)}
+          disabled={!this.state.newItem.length}
+          >Add Todo</button>
+
+          <div className="list">
+            <ul>
+              {this.state.list.map(item => {
+                return(
+                  <li key={item.id}>
+                    <input
+                    type="checkbox"
+                    name=""
+                    checked={item.isDone}
+                    onChange={()=> this.checkItem(item)}
+                    />
+                    {item.value}
+                    <button className="btn"
+                    onClick={()=>this.deleteItem(item.id)}
+                    >Delete</button>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
